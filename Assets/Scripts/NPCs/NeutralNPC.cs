9833a7ddc4366711f38f5ff8e7869014baa54abd
DaniJay10 <danielpinzon20jay@gmail.com>
2026-03-28 20:10:45 -0500

    NPCs y Sheeps

--- Assets/Scripts/NPCs/NeutralNPC.cs
+++ Assets/Scripts/NPCs/NeutralNPC.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,7 +7,6 @@ public class NeutralNPC : MonoBehaviour
 {
     public float speed = 6;
     private Rigidbody2D rb2D;
-    public Transform targetTransform;
 
     NavMeshAgent navMeshAgent;
 
@@ -25,6 +25,23 @@ public class NeutralNPC : MonoBehaviour
 
     public NPCSkin selectedSkin;
 
+
+    //Movement type
+    [Header("MovementType")]
+    public MovementType movementType;
+
+    public enum MovementType
+    {
+        Path,
+        RandomMovement
+    }
+
+
+    [Header("Path")]
+    public Transform[] pathPoints;
+    public float waitTimeInPount = 3;
+    private int indexPath = 0;
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
@@ -37,11 +54,15 @@ public class NeutralNPC : MonoBehaviour
 
         ApplySkin();
 
+        if(movementType == MovementType.Path)
+        {
+            StartCoroutine(FollowPath());
+        }
+
     }
 
     void Update()
     {
-        navMeshAgent.SetDestination(targetTransform.position);
         AdjustAnimationAndRotation();
     }
 
@@ -62,10 +83,38 @@ public class NeutralNPC : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Cambia de skin segun la seleccionada
+    /// </summary>
     public void ApplySkin()
     {
         int skinIndex = (int)selectedSkin;
 
         animator.runtimeAnimatorController = animationControllers[skinIndex];
+    }
+
+    /// <summary>
+    /// Movimiento NPC por puntos
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator FollowPath()
+    {
+        while (true)
+        {
+            if(pathPoints.Length > 0)
+            {
+                navMeshAgent.SetDestination(pathPoints[indexPath].position);
+
+                while (!navMeshAgent.pathPending && navMeshAgent.remainingDistance>0.1f)
+                {
+                    yield return null;
+                }
+
+                yield return new WaitForSeconds(waitTimeInPount);
+
+                indexPath = (indexPath + 1) % pathPoints.Length;
+
+            }
+        }
     }
 }
